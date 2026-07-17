6a2ba1668b1754e89adfecf46cb399036f9da1dc
Alexander <aleexch08@gmail.com>
2026-07-17 17:37:35 -0400

    splash screen

--- src/main/kotlin/com/example/photolab/PDIApplication.kt
+++ src/main/kotlin/com/example/photolab/PDIApplication.kt
@@ -1,27 +1,59 @@
 package com.example.photolab
 
 import javafx.application.Application
+import javafx.concurrent.Task
 import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.image.Image
 import javafx.stage.Stage
-import java.net.URL
+import javafx.stage.StageStyle
 import nu.pattern.OpenCV
+import java.net.URL
 
 class PDIApplication : Application() {
-    override fun start(stage: Stage) {
-        OpenCV.loadLocally()
-        val fxmlUrl: URL? = PDIApplication::class.java.getResource("main-view.fxml")
+    override fun start(primaryStage: Stage) {
+        val splashUrl = PDIApplication::class.java.getResource("splash-view.fxml")
+        val fxmlLoader = FXMLLoader(splashUrl)
+        val splashScene = Scene(fxmlLoader.load())
 
-        val fxmlLoader = FXMLLoader(fxmlUrl)
-        val scene = Scene(fxmlLoader.load(), 1440.0, 900.0)
+        val splashStage = Stage()
+        splashStage.initStyle(StageStyle.UNDECORATED)
+        splashStage.scene = splashScene
+
         val iconStream = PDIApplication::class.java.getResourceAsStream("icon.png")
+        if (iconStream != null) {
+            val icon = Image(iconStream)
+            splashStage.icons.add(icon)
+            primaryStage.icons.add(icon)
+        }
 
-        stage.icons.add(Image(iconStream))
-        stage.title = "PhotoLab PDI Pro"
-        stage.scene = scene
-        stage.isMaximized = true
-        stage.show()
+        splashStage.show()
+
+        val initTask = object : Task<Void?>() {
+            override fun call(): Void? {
+                OpenCV.loadLocally()
+                return null
+            }
+        }
+
+        initTask.setOnSucceeded {
+            val mainUrl: URL? = PDIApplication::class.java.getResource("main-view.fxml")
+            val mainRoot = FXMLLoader(mainUrl).load<Parent>()
+            val mainScene = Scene(mainRoot, 1440.0, 900.0)
+
+            primaryStage.title = "PhotoLab PDI Pro"
+            primaryStage.scene = mainScene
+            primaryStage.isMaximized = true
+
+            splashStage.hide()
+            primaryStage.show()
+        }
+
+        initTask.setOnFailed {
+            it.source.exception.printStackTrace()
+        }
+        Thread(initTask).start()
     }
 }
 
